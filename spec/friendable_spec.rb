5f9a65a10898da909a593d3a6ed5a4e7a9bf92f6
Yuki Nishijima <mail@yukinishijima.net>
2012-09-27 00:59:14 +0900

    Write more specs for config

--- spec/friendable_spec.rb
+++ spec/friendable_spec.rb
@@ -14,8 +14,25 @@ describe Friendable do
       its(:port) { should == 6379 }
     end
 
+    context "with a string" do
+      before { Friendable.redis = "10.0.0.1:6379:1" }
+      subject { Friendable.redis.client }
+      it { should be_a(Redis::Client) }
+      its(:host) { should == "10.0.0.1" }
+      its(:port) { should == 6379 }
+      its(:db) { should == 1 }
+    end
+
     context "with an object of Redis class" do
       before { Friendable.redis = Redis.new(:host => "127.0.0.1", :port => 6379) }
+      subject { Friendable.redis.client }
+      it { should be_a(Redis::Client) }
+      its(:host) { should == "127.0.0.1" }
+      its(:port) { should == 6379 }
+    end
+
+    context "with an object of Redis::Namespace class" do
+      before { Friendable.redis = Redis::Namespace.new(:friendable, :host => "127.0.0.1", :port => 6379) }
       subject { Friendable.redis.client }
       it { should be_a(Redis::Client) }
       its(:host) { should == "127.0.0.1" }
